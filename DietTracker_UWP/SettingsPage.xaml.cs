524d717258c38a2b6d6dddc93c4e9fd6e2e54ab9
jfranco1701 <jfranco@unomaha.edu>
2019-02-01 22:19:49 -0600

    Added sqlite database

--- DietTracker_UWP/SettingsPage.xaml.cs
+++ DietTracker_UWP/SettingsPage.xaml.cs
@@ -11,7 +11,8 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Microsoft.Data.Sqlite;
+using Windows.Storage;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,56 @@ namespace DietTracker_UWP
         public SettingsPage()
         {
             this.InitializeComponent();
+        }
+
+        void button_click(object sender, RoutedEventArgs e)
+        {
+            InitializeDatabaseAsync();
+        }
+
+
+        public static async System.Threading.Tasks.Task InitializeDatabaseAsync()
+        {
+            List<int> nums = new List<int>();
+            SqliteDataReader query;
+
+            StorageFile file;
+            try
+            {
+                file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("usda.db");
+            }
+            catch
+            {
+                StorageFile Importedfile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/usda.db"));
+                file = await Importedfile.CopyAsync(Windows.Storage.ApplicationData.Current.LocalFolder);
+            }
+
+            String path = file.Path;
+
+            using (SqliteConnection db = new SqliteConnection("Filename = " + path))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand("SELECT * FROM FoodItems WHERE NDBNum = 10000", db);
+                try
+                {
+                    query = selectCommand.ExecuteReader();
+                    while (query.Read())
+                    {
+                        nums.Add(Convert.ToInt32(query.GetString(0)));
+                    }
+                }
+                catch (SqliteException error)
+                {
+                    //Handle error
+                }
+
+                db.Close();
+            }
+
+            
+
+
+
         }
     }
 }
