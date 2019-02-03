1ecfc71e8c1a15c5dbae15bbb6a1f236d1a8a486
jfranco1701 <jfranco@unomaha.edu>
2019-02-03 14:33:12 -0600

    Created custom control to display food lists

---
+++ DietTracker_UWP/FoodListControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DietTracker_UWP.BO;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DietTracker_UWP
+{
+    public sealed partial class FoodListControl : UserControl
+    {
+        public FoodListControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); RaisePropertyChanged(); }
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable),
+                typeof(FoodListControl), new PropertyMetadata(DependencyProperty.UnsetValue));
+
+
+        void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- DietTracker_UWP/MealsPage.xaml.cs
+++ DietTracker_UWP/MealsPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DietTracker_UWP.BO;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +23,50 @@ namespace DietTracker_UWP
     /// </summary>
     public sealed partial class MealsPage : Page
     {
+        public IList<FoodItem> Items { get; set; }
+
+        List<FoodItem> lstBreakfast;
+        List<FoodItem> lstLunch;
+        List<FoodItem> lstDinner;
+        List<FoodItem> lstSnack;
+
         public MealsPage()
         {
             this.InitializeComponent();
+
+            CalPickerDate.Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+            GetMealItems();
+        }
+
+        private void GetMealItems()
+        {
+            //Get the day's meal items from the API
+            lstBreakfast = new List<FoodItem>();
+            lstLunch = new List<FoodItem>();
+            lstDinner = new List<FoodItem>();
+            lstSnack = new List<FoodItem>();
+
+            FoodItem test = new FoodItem() { FoodName = "Test food" };
+            lstBreakfast.Add(test);
+
+
+
+            //BreakfastFoodList.ItemsSource = lstBreakfast;
+
+            Items = lstBreakfast;
+            BreakfastFoodList.ItemsSource = Items;
+
+        }
+
+        void ButtonPrev_Click(object sender, RoutedEventArgs e)
+        {
+            CalPickerDate.Date = CalPickerDate.Date.Value.AddDays(-1);
+        }
+
+        void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            CalPickerDate.Date = CalPickerDate.Date.Value.AddDays(1);
         }
 
         void ButtonAddItems_Click(object sender, RoutedEventArgs e)
